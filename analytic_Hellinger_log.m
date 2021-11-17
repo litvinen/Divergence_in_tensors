4402933f39eec643815b49e133df87d3d2082fcd
Alexander Litvinenko <litvinenko@uq.rwth-aachen.de>
2021-11-17 16:19:22 +0100

    new files

---
+++ analytic_Hellinger_log.m
@@ -0,0 +1,89 @@
+% Compute analytical formula for the Hellinger distance
+
+clc;
+clear;
+
+format long
+d = 16;
+n = 128;
+r = 0.5;
+tol = 1e-12;
+% Spatial domain
+a = 100;
+
+h = 2*a/n;
+x = ((-n/2+1):(n/2))'*h;
+volume = ((max(x) - min(x))/n)^d;
+mu1 = 1.1*ones(1,d);
+mu2 = 1.4*ones(1,d);
+sigm1 = 1.5;
+sigm2 = 22.1;
+S1 = sigm1^2*eye(d);
+iS1=inv(S1);
+S2 = sigm2^2*eye(d);
+iS2=inv(S2);
+detS1 = det(S1);
+detS2 = det(S2);
+%rng('default')  % For reproducibility
+Temp1 = (r*iS1 + (1-r)*iS2);
+%KK = power(det(Temp1), -1/2);
+
+%KK = KK/(power(detS1,r/2) * power(detS2, (1-r)/2))
+
+
+[U S V] = svd(Temp1);
+Temp2 = sum(log(diag(S)));
+K = -0.5 * Temp2;
+[U eS1 V] = svd(S1);
+Temp3 = sum(log(diag(eS1)))*r/2;
+[U eS2 V] = svd(S2);
+Temp4 = sum(log(diag(eS2)))*(1-r)/2;
+
+KKK = K - (Temp3+Temp4);
+%KKK = exp(K)
+QF = (mu1 - mu2) * inv(r*S2 + (1-r) *S1) * (mu1 - mu2)'
+KKK = KKK  + (r*(r-1) * QF / 2)
+%KK = KK*exp(r*(r-1) * QF / 2)
+K=exp(KKK)
+
+DH2sq  = (1-K) 
+DH  = power ((1-K), 0.5) 
+
+
+
+
+g0 = tt_unit(n,1,n/2);
+g0 = mtkron(repmat({g0}, 1, d));
+
+
+fun_P_log = @(x)((-sum((x-mu1(:)').^2,2)/(2*sigm1^2)) -(d/2)*log(2*pi) - 0.5*sum(log(diag(eS1))));
+fun_P = @(x)exp(fun_P_log(x));
+%fun_P = @(x)exp(-sum((x-mu1(:)').^2,2)/(2*sigm1^2))/ ((2*pi)^(d/2)*sqrt(detS1));
+fun_Q_log = @(x)((-sum((x-mu2(:)').^2,2)/(2*sigm2^2)) -(d/2)*log(2*pi) - 0.5*sum(log(diag(eS2))));
+fun_Q = @(x)exp(fun_Q_log(x));
+
+%fun_Q = @(x)exp(-sum((x-mu2(:)').^2,2)/(2*sigm2^2))/ ((2*pi)^(d/2)*sqrt(detS2));
+%fun_sqHellinger = @(x) (sqrt(fun_P(x)) - sqrt(fun_Q(x))).*(sqrt(fun_P(x)) - sqrt(fun_Q(x)));
+funP_sqHellinger = @(x) (sqrt(fun_P(x)))
+funQ_sqHellinger = @(x) (sqrt(fun_Q(x)))
+
+tic
+%sqHellinger_tensor = amen_cross_s(n*ones(d,1), @(ind)fun_sqHellinger (reshape(x(ind), [], d)), tol, 'vec',true, 'y0', g0);
+P_tensor = amen_cross_s(n*ones(d,1), @(ind)funP_sqHellinger (reshape(x(ind), [], d)), tol, 'vec',true, 'y0', g0);
+Q_tensor = amen_cross_s(n*ones(d,1), @(ind)funQ_sqHellinger (reshape(x(ind), [], d)), tol, 'vec',true, 'y0', g0);
+D=P_tensor - Q_tensor;
+
+%display(sqHellinger_tensor)
+%dt=whos('sqHellinger_tensor'); 
+%MB=dt.bytes*9.53674e-7
+%sqHellinger_value = dot(tt_ones(n,d)*h^d, sqHellinger_tensor)
+%sqHellinger_value = dot(tt_ones(n,d)*h^d, D)
+sqHellinger_value = dot(D, D)*h^d
+Hellinger_TT = sqrt(sqHellinger_value/2)
+comptime = toc
+ea = abs(DH - Hellinger_TT)
+er = abs(DH - Hellinger_TT)/DH
+
+
+
+
